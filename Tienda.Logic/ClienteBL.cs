5f1ffaf52951951a1ea51ff3d8d0bd246d5c1057
ftapia@csticorp.biz <ftapia@csticorp.biz>
2022-06-09 20:35:59 -0500

    Financiera

---
+++ Tienda.Logic/ClienteBL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Financiera.Data;
+using Financiera.Core;
+namespace Tienda.Logic
+{
+    public class ClienteBL
+    {
+        public static List<Cliente> Listar()
+        {
+            using (var db = new FinancieraEntities())
+            {
+                return db.Cliente.ToList();
+            }
+        }
+
+        public static Cliente Buscar(int id)
+        {
+            using (var db = new FinancieraEntities())
+            {
+                return db.Cliente.Find(id);
+            }
+        }
+    }
+}
